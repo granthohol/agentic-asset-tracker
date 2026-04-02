f16f0afd4c9ac3fb894bfba48ebb65dde78f3ef9
Grant Hohol <ghohol@wisc.edu>
2026-04-01 20:39:13 -0500

    Utilize hooks useState and useEffect to poll the drone data from the backend every second

--- frontend/src/components/DroneMap.tsx
+++ frontend/src/components/DroneMap.tsx
@@ -1,6 +1,29 @@
 import {MapContainer, TileLayer} from 'react-leaflet';
+import { useState, useEffect } from 'react';
+
+import type { Drone } from '../types/drone'
 
 export default function DroneMap() {
+    const [drones, setDrones] = useState<Drone[]>([]);
+    useEffect(() => {
+        // This code runs exactly ONE time when the map first appears.
+        const timer = setInterval(() => {
+            
+            // Fetch the JSON from Spring Boot backend
+            fetch('http://localhost:8080/api/drones')
+                .then(response => response.json())
+                .then(data => {
+                    // It trips the useState wire, moving the drones on the map
+                    setDrones(data);
+                });
+
+        }, 1000);   // loop every 1000ms (one second)
+
+        // The Cleanup: If the user navigates away from the map, this destroys the timer.
+        return () => clearInterval(timer);
+
+    }, []); // [] -> runs once on mount, never again
+
     return (
         <MapContainer
             center={[39.0, -77.2]}
